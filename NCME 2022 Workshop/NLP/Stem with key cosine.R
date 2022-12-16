e94fd497779b36788967ef9f80782ce178a7c5d8
mpeabody77 <michael.peabody77@gmail.com>
2022-12-16 10:29:59 -0500

    Initial set up.

---
+++ NCME 2022 Workshop/NLP/Stem with key cosine.R
@@ -0,0 +1,94 @@
+#############################################
+# Basic Text Similarity Syntax              #
+#                                           #
+# by Kirk A Becker                          #
+# 2022-04-01                                #
+#                                           #
+# NCME Enemy Item Detection Workshop        #
+#############################################
+
+install.packages("tm")
+install.packages("stopwords")
+install.packages("lsa")
+install.packages("textstem")
+install.packages("stringr")
+library(tm)
+library(stringr)
+library(stopwords)
+library(lsa)
+library(textstem)
+
+#Read in item bank
+#setwd("C:\\Users\\beckka.PEROOT\\Dropbox\\NCME enemies and NLP workshop 2022\\Test Similarity")
+items<-read.table("item examples for similarity.txt",header=TRUE,fill=TRUE,sep="\t")
+stoplist<-read.delim("stoplist.txt")
+
+#For any component of the stem, we'll want to clean out the punctuation,
+#numbers, white space, and pesky special characters
+stem<-removePunctuation(items$Stem)
+stem<-tolower(stem)
+stem<-removeNumbers(stem)
+stem<-str_replace(stem,"\t"," ")
+stem<-str_replace(stem,"\n"," ")
+stem<-stripWhitespace(stem)
+
+#Create a matrix with a column for item id and a column for each word in the
+#item component. I'm separately lemmatizing and stemming each word here. 
+#Larger/more complex document sets might identify strengths of the different
+#options (e.g., stemming might be better or wors for highly specific text)
+idword<-matrix("",ncol=4)
+colnames(idword)<-c("ID","word","stem","lemma")
+idword<-idword[-1,]
+
+for(i in 1:length(stem)){
+ sp<-stem[i]
+ sp<-str_split(sp," ")
+#stem words - I believe the default here is specific to English
+ sp2<-stemDocument(stem[i])
+ sp2<-str_split(sp2," ")
+#lemmatize words
+ sp3<-lemmatize_strings(stem[i])
+ sp3<-str_split(sp3," ")
+ idword<-rbind(idword,cbind(rep(paste("stem_",items$ItemID[i],sep=""),length(sp2[[1]])),sp[[1]],sp2[[1]],sp3[[1]]))
+}
+
+
+#repeat for key
+key<-removePunctuation(items$Key)
+key<-tolower(key)
+key<-removeNumbers(key)
+key<-str_replace(key,"\t"," ")
+key<-str_replace(key,"\n"," ")
+key<-stripWhitespace(key)
+
+
+for(i in 1:length(key)){
+ sp<-key[i]
+ sp<-str_split(sp," ")
+#stem words - I believe the default here is specific to English
+ sp2<-stemDocument(key[i])
+ sp2<-str_split(sp2," ")
+#lemmatize words
+ sp3<-lemmatize_strings(key[i])
+ sp3<-str_split(sp3," ")
+ idword<-rbind(idword,cbind(rep(paste("key_",items$ItemID[i],sep=""),length(sp2[[1]])),sp[[1]],sp2[[1]],sp3[[1]]))
+}
+
+idword<-subset(idword,idword[,2]!="")
+for(j in 1:nrow(stoplist)){
+ idword<-subset(idword,idword[,2]!=stoplist[j,1])
+}
+
+#Create a document term matrix
+dwi<-table(idword[,1],idword[,3])
+dwicos<-cosine(t(dwi))
+stempairs<-data.frame(rep(rownames(dwicos),times=ncol(dwicos)),stack(data.frame(dwicos)))
+
+#note - this will only work if the item ids don't contain spaces
+stempairs<-subset(stempairs,stempairs[,1]!=stempairs[,3])
+ptype<-matrix(substr(stempairs[,1],1,3),ncol=1)
+stempairs<-subset(stempairs,ptype=="key")
+
+write.table(stempairs,"key stem pairs.csv",sep=",",append=FALSE,row.names=FALSE,col.names=TRUE)
+
+
